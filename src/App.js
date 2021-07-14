3388cc3548d358f4d7747ceecf6e08caa96177a7
martin suarez <msuarez@webcreek.com>
2021-07-14 14:17:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Water = () => {
+    const [degrees, setDegrees] = useState('');
+    const [waterState, setWaterState] = useState('NOTHING');
+    const [classNameState, setClassNameState] = useState('liquid');
 
-export default App;
+    const handleChange = e => {
+        const { value } = e.target;
+        console.log(value, 'vallll');
+        let result = parseInt(value);
+        setDegrees(result);
+        console.log(degrees);
+        if (value <= 0) {
+            setWaterState('Solid');
+            setClassNameState('solid');
+        } else if (value >= 100) {
+            setWaterState('Gas');
+            setClassNameState('gas');
+        } else {
+            setWaterState('Liquid');
+            setClassNameState('liquid');
+        }
+    };
+
+    return (
+        <div className="WaterApp">
+            <h2>Degrees:</h2>{' '}
+            <input
+                type="number"
+                name="water"
+                value={degrees}
+                onChange={handleChange}
+                required
+            />
+            <div className={classNameState}>
+                <h3>
+                    At {degrees}°C, water is considered to be a "{waterState}" state of
+                    matter.
+                </h3>
+            </div>
+        </div>
+    );
+};
+
+export default Water;
